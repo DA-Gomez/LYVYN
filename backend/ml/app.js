91a7edac624f6fb9053b4860db79801e2a9f2f71
menal00 <menalmesuod00@gmail.com>
2026-03-11 23:55:51 -0400

    Add clothes API backend

--- backend/ml/app.js
+++ backend/ml/app.js
@@ -1,5 +1,6 @@
 
 import express from "express";
+import { number } from "yargs";
 const server = express();
 
 server.use(express.json());
@@ -8,7 +9,7 @@ const clothes = [
 {
   id: 1, 
 category: "shoes",
- type: "lather",
+ type: "leather",
  warmth: "medium",
  formality: "formal",
 colorGroup:"warm"
@@ -80,8 +81,27 @@ function getclothes(id){
 
 
 function addclothes(cloth){
-	clothes.push({cloth});
-		return cloth;
+  for(let i = 0; i < clothes.length ; i++){
+    if (clothes[i].id !== number & clothes[i].id !== any(clothes.id)){
+      return false;
+    }if (clothes[i].category !== any(clothes.category)){
+      return false;
+    }if (clothes[i].type !== any(clothes.type)){
+      return false;
+    }if (clothes[i].warmth !== any(clothes.warmth)){
+      return false;
+    }if (clothes[i].formality !== any(clothes.formality)){
+      return false;
+    }if (clothes[i].colorGroup !==  any(clothes.colorGroup)){
+      return false;
+    }
+  }
+  clothes.push(cloth);
+  return true;
+}
+
+function recommendClothes(UserInput){
+
 }
 
 
@@ -94,9 +114,23 @@ server.get("/clothes", (req, res) => {
 server.post("/clothes",(req, res) => {
 	console.log("POST /clothes was called");
   const newCloth = addclothes(req.body);
-  res.status(201).json(newCloth);
+
+if (newCloth === true){
+res.status(201).json(newCloth);
+}
+else {
+res.status(400).json({ error: "Invalid cloth data" });
+} 
 });
 
 server.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
+});
+
+server.post("/recommend", (req,res) => {
+const input = req.body;
+
+
+
+
 });
