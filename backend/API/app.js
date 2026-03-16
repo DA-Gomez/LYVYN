4e20c25f8b60af7ebed45f1b783949c64946ec8c
menal00 <menalmesuod00@gmail.com>
2026-03-16 18:40:01 -0400

    update backend outfit recommendation

--- backend/API/app.js
+++ backend/API/app.js
@@ -64,10 +64,111 @@ colorGroup: "warm"
  warmth:  "heavy",
  formality: "casual",
 colorGroup:  "cool"
-}
-
-];
-
+},
+{
+  id: 8,
+  category: "shoes",
+  type: "sneakers",
+  warmth: "medium",
+  formality: "casual",
+  colorGroup: "cool"
+},
+{
+  id: 9,
+  category: "top",
+  type: "hoodie",
+  warmth: "heavy",
+  formality: "casual",
+  colorGroup: "warm"
+},
+{
+  id: 10,
+  category: "top",
+  type: "hoodie",
+  warmth: "light",
+  formality: "casual",
+  colorGroup: "neutral"
+},
+{
+  id: 11,
+  category: "top",
+  type: "leather",
+  warmth: "medium",
+  formality: "formal",
+  colorGroup: "cool"
+},
+{
+  id: 12,
+  category: "bottom",
+  type: "jeans",
+  warmth: "light",
+  formality: "casual",
+  colorGroup: "neutral"
+},
+{
+  id: 13,
+  category: "bottom",
+  type: "jeans",
+  warmth: "medium",
+  formality: "formal",
+  colorGroup: "cool"
+},
+{
+  id: 14,
+  category: "bottom",
+  type: "jeans",
+  warmth: "heavy",
+  formality: "formal",
+  colorGroup: "warm"
+},
+{
+  id: 15,
+  category: "shoes",
+  type: "leather",
+  warmth: "medium",
+  formality: "formal",
+  colorGroup: "neutral"
+},
+{
+  id: 16,
+  category: "shoes",
+  type: "sneakers",
+  warmth: "light",
+  formality: "casual",
+  colorGroup: "cool"
+},
+{
+  id: 17,
+  category: "shoes",
+  type: "sneakers",
+  warmth: "heavy",
+  formality: "casual",
+  colorGroup: "warm"
+},
+{
+  id: 18,
+  category: "outerwear",
+  type: "hoodie",
+  warmth: "heavy",
+  formality: "casual",
+  colorGroup: "neutral"
+},
+{
+  id: 19,
+  category: "outerwear",
+  type: "leather",
+  warmth: "medium",
+  formality: "formal",
+  colorGroup: "cool"
+},
+{
+  id: 20,
+  category: "outerwear",
+  type: "hoodie",
+  warmth: "light",
+  formality: "casual",
+  colorGroup: "warm"
+}];
 
 
 function getclothes(){
@@ -77,12 +178,12 @@ function getclothes(){
 
 function addclothes(newCloth){
   
-  const validCategories = ["shoes", "bottom", "outerwear", "top"];
-  const validWarmth = ["light", "medium", "heavy"];
-  const validFormality = ["formal", "casual"];
-  const validColorGroups = ["warm", "cool", "neutral"];
-  const validTypes = ["leather", "hoodie", "jeans"];
-  const UsedId = [1, 2, 3, 4, 5, 6, 7];
+  const validCategories = clothes.map(clothItem => clothItem.category);
+  const validWarmth = clothes.map(clothItem => clothItem.warmth);
+  const validFormality = clothes.map(clothItem => clothItem.formality);
+  const validColorGroups = clothes.map(clothItem => clothItem.colorGroup);
+  const validTypes = clothes.map(clothItem => clothItem.type);
+  const UsedId = clothes.map(clothItem => clothItem.id);
 
  
     if(!newCloth){
@@ -101,8 +202,7 @@ function addclothes(newCloth){
     }if (!validColorGroups.includes(newCloth.colorGroup)){
       return false;
     }
-  
-  
+
   clothes.push(newCloth);
   return true;
 }
@@ -127,10 +227,72 @@ server.get("/clothes", (req, res) => {
 });
 
 
-
 server.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
+
+
+function getOutfits(filteredClothes, outWear){
+
+ var tops = filteredClothes.filter(c => c.category === "top");
+      var bottoms = filteredClothes.filter(c => c.category === "bottom");
+      var shoes = filteredClothes.filter(c => c.category === "shoes");
+      var outerwear = filteredClothes.filter(c => c.category === "outerwear");
+
+      const outfits = [];
+
+  if (outWear){
+      for (let t of tops){
+        for (let b of bottoms){
+          for (let s of shoes){
+            for (let o of outerwear){
+              outfits.push({ top: t, bottom: b, shoes: s, outerwear: o });
+            }
+          }
+        }
+      }
+    }
+  else {
+      for (let t of tops){
+        for (let b of bottoms){
+          for (let s of shoes){
+              outfits.push({ top: t, bottom: b, shoes: s });
+          }
+        }
+      }
+  }
+  return outfits;
+}
+
+
+
+
+
+function ruleFiltering(weather, occasion, listOfClothes){
+
+ var filteredClothes = clothes.filter(c => listOfClothes.includes(c.id));
+
+ var outWear = false;
+
+if(weather === "cold" && occasion === "formal"){
+  filteredClothes = filteredClothes.filter(c => (c.warmth === "heavy" || c.warmth === "medium") && c.formality === "formal");
+  outWear = true;
+}
+else if(weather === "hot" && occasion === "formal"){
+  filteredClothes = filteredClothes.filter(c => (c.warmth === "light" || c.warmth === "medium") && c.formality === "formal");
+}
+else if(weather === "cold" && occasion === "casual"){
+  filteredClothes = filteredClothes.filter(c => (c.warmth === "heavy" || c.warmth === "medium") && c.formality === "casual");
+  outWear = true;
+}
+else if(weather === "hot" && occasion === "casual"){
+  filteredClothes = filteredClothes.filter(c => (c.warmth === "light" || c.warmth === "medium") && c.formality === "casual");
+}
+
+
+ return {filteredClothes , outWear};
+
+}
 
 
 function recommendClothes(UserInput){
@@ -142,42 +304,23 @@ function recommendClothes(UserInput){
 var weather = UserInput.weather;
 var occasion = UserInput.occasion;
 var listOfClothes = UserInput.listOfClothes;
-var recommendedOutfit = [];
 
-if (weather === "hot" && occasion === "casual"){
-  for (let i = 0; i < clothes.length; i++){
-    if (clothes[i].warmth === "light" && clothes[i].formality === "casual" && listOfClothes.includes(clothes[i].category)){
-      recommendedOutfit.push(clothes[i]);
-    }
-  }
+
+var {filteredClothes: clothesList, outWear } = ruleFiltering(weather, occasion, listOfClothes);
+
+var outfit = getOutfits(clothesList, outWear);
+
+if (outfit.length === 0) {
+  return { message: "No match found" };
 }
-else if (weather === "hot" && occasion === "formal"){
-  for (let i = 0; i < clothes.length; i++){
-    if (clothes[i].warmth === "light" && clothes[i].formality === "formal" && listOfClothes.includes(clothes[i].category)){
-      recommendedOutfit.push(clothes[i]);
-    }
-  }
-}
-else if (weather === "cold" && occasion === "casual"){
-  for (let i = 0; i < clothes.length; i++){
-    if ((clothes[i].warmth === "heavy" || clothes[i].warmth === "medium") && clothes[i].formality === "casual" && listOfClothes.includes(clothes[i].category)){
-      recommendedOutfit.push(clothes[i]);
-    }
-  }
-}
-else if (weather === "cold" && occasion === "formal"){
-  for (let i = 0; i < clothes.length; i++){
-    if ((clothes[i].warmth === "heavy" || clothes[i].warmth === "medium") && clothes[i].formality === "formal" && listOfClothes.includes(clothes[i].category)){
-      recommendedOutfit.push(clothes[i]);
-    }
-  }
-} 
-return recommendedOutfit;
+
+return outfit;
 
 }
+
 
 server.post("/recommend", (req,res) => {
-  console.log("POST /recommend was called");
+console.log("POST /recommend was called");
 const recOutfit = recommendClothes(req.body)
 
 if (recOutfit === false){
